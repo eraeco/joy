2549d594bcb580462fee26b0bf22b904339479ec
Mark Nadal <mark@gun.eco>
2022-07-13 01:35:31 -0700

    fix css serve

--- start.js
+++ start.js
@@ -1,6 +1,23 @@
 ;(function(){
 	// update temporarily for Secure Render:
-	require('http').createServer(require('gun').serve(__dirname)).listen(8769);
+
+	var http = require('http'), fs = require('fs'), path = require('path');
+
+	http.createServer(function(req, res){
+		if(res.setHeader){ res.setHeader('Access-Control-Allow-Origin', '*') }
+		if(!req.url){ return res.end(404+'') }
+		var dir = path.resolve(), file = path.join(dir, req.url);
+		if(file.slice(0, dir.length) != dir){ return res.end(404+'') }
+		var ext = file.split('.').slice(-1)[0];
+		if(ext == 'js'){ res.writeHead(200, {'Content-Type': 'text/javascript'}) }
+		if(ext == 'css'){ res.writeHead(200, {'Content-Type': 'text/css'}) }
+		if(ext == 'html'){ res.writeHead(200, {'Content-Type': 'text/html'}) }
+		var stream = fs.createReadStream(file);
+		stream.on('error', function(err){ res.end(404+'') });
+		stream.pipe(res);
+	}).listen(8769);
+	console.log(":8769");
+
 	return;
 	var gun = require('gun/examples/http');
 	if(!gun.back){ return } // http example auto spawns subprocess
