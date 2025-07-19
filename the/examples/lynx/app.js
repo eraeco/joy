09b0ff72c0f09137e51b8a5d9a4f7887b43909ca
Natnael Teferi <51678624+aethiop@users.noreply.github.com>
2025-07-19 15:39:18 -0700

    make the.view API usable

--- the/examples/lynx/app.js
+++ the/examples/lynx/app.js
@@ -1,31 +1,20 @@
 import "./the.js"
 
-the.place({
-  fill: [0, 1, 0, 1],
-  size: [[100, '.'], [100, '.']],
-}).into(the.view);
+var b = the.view({
+  fill: "Hello, from B!",
+  size: [5]
+})
 
-the.place({
-  fill: [1, 1, 0, 1],
-  size: [[100, '.'], [100, '.']],
-}).into(the.view);
-
-the.place({
-  fill: [1, 0, 0, 1],
-  size: [[100, '.'], [100, '.']]
-}).into(the.view);
-
-the.place({
-  fill: "Hello from THE!",
+var c = the.view({
+  fill: "Hello, from C!",
   size: [2]
-}).into(the.view);
+})
 
-the.place({
-  fill: "Hello from THE!",
-  size: [2]
-}).into(the.view);
+var d = the.view({
+  fill: "Hello, from D!",
+  size: [1]
+})
 
-the.place({
-  fill: "Hello from THE!",
-  size: [3]
-}).into(the.view);
+b.into();
+c.into(b);
+d.into();
